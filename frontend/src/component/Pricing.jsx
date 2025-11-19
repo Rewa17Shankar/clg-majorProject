9feaf266eff007a35905afc5729b6a537c1dfd8e
Rewa17Shankar <shankarewajii@gmail.com>
2025-11-19 23:25:10 +0530

    Frontend-API's

--- frontend/src/component/Pricing.jsx
+++ frontend/src/component/Pricing.jsx
@@ -103,7 +103,8 @@ export default function Pricing() {
         phone: buyer.phone
       };
 
-      const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000" || "https://clg-majorproject-oab0.onrender.com";
+      const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000" || 
+      "https://clg-majorproject-oab0.onrender.com";
       const res = await fetch(`${API_BASE}/api/payment`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
